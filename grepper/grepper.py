d1ff9bd8235f28223c7975290e48d3164c82f6ba
Paul Weaver <paul.weaver@osirium.com>
2016-09-02 13:45:32 +0100

    Handle subprocess error more gracefully

--- grepper/grepper.py
+++ grepper/grepper.py
@@ -2,17 +2,21 @@
 import sys
 import os
 from functools import partial
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
 from selector.selector import select_from_list
 
 if __name__ == '__main__':
-    lines = check_output(['git', 'grep', '-n'] + sys.argv[1:]).splitlines()
-    lines = tuple(map(partial(bytes.decode, encoding='utf-8'), lines))
-    selection = select_from_list(lines)
-    path, line_number, match = selection.selected.split(':', maxsplit=2)
-    args = os.environ.get('EDITOR', 'vim').split()
-    args.append('+{}'.format(line_number))
-    args.append(path)
-    args.append(os.environ)
-    os.execlpe(args[0], *args)
+    try:
+        lines = check_output(['git', 'grep', '-n'] + sys.argv[1:]).splitlines()
+    except CalledProcessError as e:
+        sys.exit(1)
+    else:
+        lines = tuple(map(partial(bytes.decode, encoding='utf-8'), lines))
+        selection = select_from_list(lines)
+        path, line_number, match = selection.selected.split(':', maxsplit=2)
+        args = os.environ.get('EDITOR', 'vim').split()
+        args.append('+{}'.format(line_number))
+        args.append(path)
+        args.append(os.environ)
+        os.execlpe(args[0], *args)
